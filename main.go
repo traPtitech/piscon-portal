61c6311474857428148180939f349e8b133cbe2b
tohutohu <tohu.soy@gmail.com>
2018-06-27 02:52:13 +0900

    bench

--- main.go
+++ main.go
@@ -4,23 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"os/exec"
-	"sync"
 	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	shellwords "github.com/mattn/go-shellwords"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 var (
-	queingCh     chan *Task
-	taskQue      []*Task
-	taskQueMutex *sync.Mutex
-	endTask      chan struct{}
-	sendWoker    chan *Task
-	db           *gorm.DB
+	checkTask  chan struct{}
+	sendWorker chan *Task
+	db         *gorm.DB
 )
+
+type jwtCustomClaims struct {
+	Name  string `json:"name"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
 
 type Output struct {
 	Pass     bool     `json:"pass"`
@@ -32,19 +39,20 @@ type Output struct {
 
 type Team struct {
 	gorm.Model
-	Name    string   `json:"name"`
-	Results []Result `json:"results"`
+	Name    string    `gorm:"unique" json:"name"`
+	Results []*Result `json:"results"`
 }
 
 type Result struct {
-	ID        int       `gorm:"AUTO_INCREMENT" json:"id"`
-	TeamID    uint      `json:"team_id"`
-	Pass      bool      `json:"pass"`
-	Score     int64     `json:"score"`
-	Suceess   int64     `json:"suceess"`
-	Fail      int64     `json:"fail"`
-	Messages  []Message `gorm:"auto_preload" json:"messages"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int        `gorm:"AUTO_INCREMENT" json:"id"`
+	TeamID    uint       `json:"team_id"`
+	TaskID    uint       `json:"task_id"`
+	Pass      bool       `json:"pass"`
+	Score     int64      `json:"score"`
+	Suceess   int64      `json:"suceess"`
+	Fail      int64      `json:"fail"`
+	Messages  []*Message `json:"messages"`
+	CreatedAt time.Time  `json:"created_at"`
 }
 
 type Message struct {
@@ -54,97 +62,156 @@ type Message struct {
 }
 
 type Task struct {
-	Cmd  *exec.Cmd `json:"-"`
-	IP   string    `json:"ip"`
-	Team Team      `json:"team"`
+	gorm.Model
+	CmdStr string `json:"cmd_str"`
+	IP     string `json:"ip"`
+	State  string `json:"state"`
+	TeamID uint   `json:"team_id"`
+	Team   Team   `json:"team"`
 }
 
 func main() {
-	queingCh = make(chan *Task)
-	taskQue = make([]*Task, 0)
-	taskQueMutex = &sync.Mutex{}
+	sendWorker = make(chan *Task, 10)
+	checkTask = make(chan struct{})
 
-	_db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
+	go benchmarkWorker()
+
+	_db, err := gorm.Open("sqlite3", "./db.db")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	db = _db
 
+	db.AutoMigrate(&Message{}, &Result{}, &Team{}, &Task{})
+	team := Team{Name: "to-hutohu"}
+	if db.NewRecord(team) {
+		db.Create(&team)
+	}
+
+	tasks := []*Task{}
+	db.Not("state = 'done'").Find(&tasks)
+	for _, t := range tasks {
+		go func(task *Task) {
+			sendWorker <- task
+		}(t)
+	}
+
 	e := echo.New()
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	e.Pre(middleware.HTTPSNonWWWRedirect())
+
+	signingKey := os.Getenv("JWTKey")
+	config := middleware.JWTConfig{
+		Claims:     &jwtCustomClaims{},
+		SigningKey: []byte(signingKey),
+	}
+	e.Use(middleware.JWTWithConfig(config))
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			user := c.Get("user").(*jwt.Token)
+			claims := user.Claims.(*jwtCustomClaims)
+			if claims.Name != "traP-showcase" {
+				return c.NoContent(http.StatusForbidden)
+			}
+			return next(c)
+		}
+	})
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
 
 	e.POST("/benchmark/run", queBenchmark)
+	e.GET("/benchmark/queue", getBenchmarkQueue)
+
+	api := e.Group("/api")
+	api.GET("/results", getAllResults)
+
+	e.StartAutoTLS(":443")
+	fmt.Println("end")
+}
+
+func getAllResults(c echo.Context) error {
+	teams := []*Team{}
+	db.Find(&teams)
+	for _, team := range teams {
+		db.Model(team).Related(&team.Results)
+		for _, result := range team.Results {
+			db.Model(result).Related(&result.Messages)
+		}
+	}
+	return c.JSON(http.StatusOK, teams)
 }
 
 func queBenchmark(c echo.Context) error {
 	requestBody := &struct {
-		IP string
+		IP string `json:"ip"`
 	}{}
 	c.Bind(requestBody)
 
+	teamID := 1
+
+	task := &Task{}
+
+	//db.Where("team_id = ?", teamID).Not("state = 'done'").First(task)
+	fmt.Println(teamID)
+	if task.CmdStr != "" {
+		return c.JSON(http.StatusNotAcceptable, map[string]string{"massage": "すでに登録されています"})
+	}
+
+	cmdStr := fmt.Sprintf("/home/benchmarker/bin/benchmarker -t http://%s -u /home/benchmarker/private-isu/benchmarker/userdata", requestBody.IP)
+	t := &Task{
+		CmdStr: cmdStr,
+		IP:     requestBody.IP,
+		State:  "waiting",
+		TeamID: 1,
+	}
+	db.Create(t)
+
 	go func() {
-		cmd := exec.Command(fmt.Sprintf("/home/benchmarker/bin/benchmarker -t %s -d /home/benchmarker/private-isu/benchmarker/userdata", requestBody.IP))
-		t := &Task{
-			Cmd:  cmd,
-			IP:   requestBody.IP,
-			Team: Team{},
-		}
-		queingCh <- t
+		sendWorker <- t
 	}()
 
 	return c.NoContent(http.StatusNoContent)
 }
 
-func getTaskQueInfo() []Team {
-	taskQueMutex.Lock()
-	teams := make([]Team, 0)
-	for _, t := range taskQue {
-		teams = append(teams, t.Team)
+func getBenchmarkQueue(c echo.Context) error {
+	tasks := getTaskQueInfo()
+	for _, task := range tasks {
+		db.Model(task).Related(&task.Team)
 	}
-	taskQueMutex.Unlock()
-	return teams
+	return c.JSON(http.StatusOK, tasks)
 }
 
-func taskManager() {
-	for {
-		select {
-		case task := <-queingCh:
-			taskQueMutex.Lock()
-			taskQue = append(taskQue, task)
-			taskQueMutex.Unlock()
-
-		case <-endTask:
-			taskQueMutex.Lock()
-			if len(taskQue) > 0 {
-				sendWoker <- taskQue[0]
-				taskQue = taskQue[1:]
-
-			}
-			taskQueMutex.Unlock()
-		}
-	}
+func getTaskQueInfo() []*Task {
+	tasks := []*Task{}
+	db.Table("tasks").Joins("LEFT JOIN teams ON `teams`.id = `tasks`.team_id").Not("state = 'done'").Find(&tasks)
+	return tasks
 }
 
 func benchmarkWorker() {
 	for {
-		task := <-sendWoker
-		err := task.Cmd.Run()
-		if err != nil {
-			panic(err)
-		}
+		task := <-sendWorker
+		fmt.Println("recieve task")
+		task.State = "benchmark"
+		db.Save(task)
 
-		output, err := task.Cmd.Output()
-		if err != nil {
-			panic(err)
-		}
+		command, _ := shellwords.Parse(task.CmdStr)
 
+		output, _ := exec.Command(command[0], command[1:]...).CombinedOutput()
+		fmt.Println("end benchmark")
+
+		fmt.Println(string(output))
 		data := &Output{}
-		err = json.Unmarshal(output, data)
+		err := json.Unmarshal(output, data)
 		if err != nil {
 			panic(err)
 		}
 		result := &Result{
-			TeamID:  task.Team.ID,
+			TeamID:  task.TeamID,
+			TaskID:  task.ID,
 			Pass:    data.Pass,
 			Score:   data.Score,
 			Suceess: data.Suceess,
@@ -152,9 +219,12 @@ func benchmarkWorker() {
 		}
 
 		for _, message := range data.Messages {
-			result.Messages = append(result.Messages, Message{Text: message})
+			result.Messages = append(result.Messages, &Message{Text: message})
 		}
 		db.Create(result)
-		endTask <- struct{}{}
+
+		task.State = "done"
+		db.Save(task)
+
 	}
 }
