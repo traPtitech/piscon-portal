cb5df4bbc8ce75dc47ed2de83546e007088614a6
nagatech <cheesecakebot@yahoo.co.jp>
2019-05-30 20:00:12 +0900

    Add articles

--- client/src/components/articles/Articles.vue
+++ client/src/components/articles/Articles.vue
@@ -1,10 +1,21 @@
 <template>
 <div>
   <vuestic-widget class="col-md-12" headerText="攻略記事一覧">
+    <div>
+      <p>今回はpixiv様の社内ISUCONの問題を使わせていただきました。この場を借りてお礼申し上げます。</p>
+    </div>
     <div class="box">
-      <h3>工事中</h3>
-      <p>気が向いたら更新します</p>
-      <p>まずは自分の力で頑張りましょう！</p>
+      <ul>
+        <li><a href="http://dsas.blog.klab.org/archives/pixiv-isucon2016-1.html" target="_blank">pixiv private isucon 2016 攻略 (1/5) - DSAS開発者の部屋</a></li>
+        <li><a href="http://dsas.blog.klab.org/archives/pixiv-isucon2016-2.html" target="_blank">pixiv private isucon 2016 攻略 (2/5) - DSAS開発者の部屋</a></li>
+        <li><a href="http://dsas.blog.klab.org/archives/pixiv-isucon2016-3.html" target="_blank">pixiv private isucon 2016 攻略 (3/5) - DSAS開発者の部屋</a></li>
+        <li><a href="http://dsas.blog.klab.org/archives/pixiv-isucon2016-4.html" target="_blank">pixiv private isucon 2016 攻略 (4/5) - DSAS開発者の部屋</a></li>
+        <li><a href="http://dsas.blog.klab.org/archives/pixiv-isucon2016-5.html" target="_blank">pixiv private isucon 2016 攻略 (5/5) - DSAS開発者の部屋</a></li>
+        <li><a href="https://recruit-tech.co.jp/blog/2017/09/07/rtech_isucon/" target="_blank">社内ISUCONに参加した話 - リクルートテクノロジーズ メンバーズブログ</a></li>
+        <li><a href="https://utgwkk.hateblo.jp/entry/2017/09/10/181130" target="_blank">pixiv社内ISUCONを解いた - 私が歌川です</a></li>
+        <li><a href="https://tech.recruit-mp.co.jp/event/post-15147/" target="_blank">社内ISUCONを開催しました！ - PSYENCE:MEDIA</a></li>
+        <li><a href="https://nippo.wikihub.io/@cheeseinmisosoup/20160911152955" target="_blank">ISUCON練習 - cheeseinmisosoup 日報</a></li>
+      </ul>
     </div>
   </vuestic-widget>
 </div>
