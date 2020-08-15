509fad7c0d46e0a3c7b5d34713fde68d9146a9cc
Hosshii <you@example.com>
2020-08-15 14:19:43 +0000

    fix

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // axios.defaults.withCredentials = true
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'http://160.251.13.26/'
-// axios.defaults.baseURL = 'http://118.27.18.240'
+// axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'http://160.251.13.26/'
+axios.defaults.baseURL = 'https://piscon-portal.trap.jp/'
 
 export default axios
