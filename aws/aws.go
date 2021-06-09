cdc0300272d83b56625bacecc6cd3f003fa1576d
hijiki51 <hibiki0719euph@gmail.com>
2021-06-10 01:21:10 +0900

    add create instace

--- aws/aws.go
+++ aws/aws.go
@@ -7,6 +7,16 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
+type EC2CreateInstanceAPI interface {
+	RunInstances(ctx context.Context,
+		params *ec2.RunInstancesInput,
+		optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error)
+
+	CreateTags(ctx context.Context,
+		params *ec2.CreateTagsInput,
+		optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error)
+}
+
 type AwsClient struct {
 	Client *ec2.Client
 }
@@ -20,4 +30,12 @@ func New() *AwsClient {
 	client := ec2.NewFromConfig(cfg)
 	a.Client = client
 	return a
+}
+
+func (a *AwsClient) CreateInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.RunInstancesInput) (*ec2.RunInstancesOutput, error) {
+	return api.RunInstances(c, input)
+}
+
+func (a *AwsClient) CreateTags(c context.Context, api EC2CreateInstanceAPI, input *ec2.CreateTagsInput) (*ec2.CreateTagsOutput, error) {
+	return api.CreateTags(c, input)
 }
