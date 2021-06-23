8b8a8529c3e37699529aae6660b5b5f020072a4d
hijiki51 <hibiki0719euph@gmail.com>
2021-06-24 00:38:11 +0900

    init buestic-admin

--- old-client/src/services/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-// axios.defaults.withCredentials = true
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://piscon-portal.trap.jp/'
-// axios.defaults.baseURL = 'https://piscon-portal.trap.jp/'
-
-export default axios
