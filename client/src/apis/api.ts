43a7c95056b84cc99127d53ebc04cae76d7fa17b
hijiki51 <hibiki0719euph@gmail.com>
2021-06-25 17:38:56 +0900

    add apis

--- client/src/apis/api.ts
+++ client/src/apis/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { randomString, pkce } from './utils'
 export const traQBaseURL = 'https://q.trap.jp/api/v3'
 export const traQClientID = 'J0RR7Auk9OVa4LZnQ4pD37hupkEkYloEHiIU'
-
+/* eslint-disable @typescript-eslint/camelcase */
 export function setAuthToken(token: string) {
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
@@ -20,7 +20,6 @@ export async function redirectAuthorizationEndpoint() {
 
   const authorizationEndpointUrl = new URL(`${traQBaseURL}/oauth2/authorize`)
   authorizationEndpointUrl.search = new URLSearchParams({
-    /* eslint-disable @typescript-eslint/camelcase */
     client_id: traQClientID,
     response_type: 'code',
     code_challenge: codeChallenge,
@@ -28,4 +27,52 @@ export async function redirectAuthorizationEndpoint() {
     state
   }).toString()
   window.location.assign(authorizationEndpointUrl.toString())
+}
+
+export function fetchAuthToken(code: string, verifier: string) {
+  return axios.post(
+    `${traQBaseURL}/oauth2/token`,
+    new URLSearchParams({
+      client_id: traQClientID,
+      grant_type: 'authorization_code',
+      code_verifier: verifier,
+      code
+    })
+  )
+}
+
+export function revokeAuthToken(token: string) {
+  return axios.post(
+    `${traQBaseURL}/oauth2/revoke`,
+    new URLSearchParams({ token })
+  )
+}
+
+export function getMe() {
+  return axios.get(`${traQBaseURL}/users/me`)
+}
+
+export function getMeGroup() {
+  return axios.get(`${traQBaseURL}/users/me/groups`)
+}
+
+export function getRsults() {
+  return axios.get(`/api/results`)
+}
+
+export function getNewer() {
+  return axios.get(`/api/newer`)
+}
+
+export function getTeam(id) {
+  return axios.get(`/api/team/${id}`)
+}
+
+export function getUser(id) {
+  return axios.get(`/api/user/${id}`)
+}
+
+export function getQueue() {
+  // TODO: Fix
+  return axios.get(`/api/benchmark/queue`)
 }
