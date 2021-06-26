be04740d52280dee59c92045b4c2938b3e103200
hijiki51 <hibiki0719euph@gmail.com>
2021-06-26 19:01:12 +0900

    add some state

---
+++ client/src/pages/admin/dashboard/dashboard-cards/Ranking.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="va-table-responsive">
+    <table class="va-table va-table--hoverable">
+      <thead>
+      <tr>
+        <th>RANK</th>
+        <th>TEAM</th>
+        <th>SCORE</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{user.fullName}}</td>
+        <td>{{user.email}}</td>
+        <td>{{user.country}}</td>
+        <td>
+          <va-badge
+            :text="user.status"
+            :color="user.status"
+          />
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script lang="ts">
+import users from 'vuestic-ui/src/data/Users'
+export default {
+  data () {
+    return {
+      users: users.slice(0, 4),
+    }
+  },
+}
+</script>
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,12 +1,17 @@
 import { createDirectStore } from 'direct-vuex'
-import { User, OAuth2Token, MyUserDetail } from '@traptitech/traq'
+import { OAuth2Token, MyUserDetail } from '@traptitech/traq'
 import TraqApis from '@/lib/apis/traq'
 import { setAuthToken } from '@/lib/apis/api'
+import apis, { Task, Team } from '@/lib/apis'
 
 const { store, rootActionContext } = createDirectStore({
   state: {
     me: null as MyUserDetail | null,
-    authToken: null as OAuth2Token | null
+    authToken: null as OAuth2Token | null,
+    Team: null as Team | null,
+    AllResults: null as Team[] | null,
+    Queue: null as Task[] | null,
+    Newer: null as Team[] | null
   },
   mutations: {
     setMe(state, me: MyUserDetail) {
@@ -15,6 +20,18 @@ const { store, rootActionContext } = createDirectStore({
     setToken(state, data: OAuth2Token) {
       state.authToken = data
       setAuthToken(data)
+    },
+    setTeam(state, data: Team) {
+      state.Team = data
+    },
+    setAllResults(state, data: Team[]) {
+      state.AllResults = data
+    },
+    setQueue(state, data: Task[]) {
+      state.Queue = data
+    },
+    setNewer(state, data: Team[]) {
+      state.Newer = data
     }
   },
   actions: {
@@ -22,6 +39,12 @@ const { store, rootActionContext } = createDirectStore({
       const { commit } = rootActionContext(context)
       const { data } = await TraqApis.getMe()
       commit.setMe(data)
+    },
+    async getData(context) {
+      const { commit } = rootActionContext(context)
+      apis.resultsGet().then(data => commit.setAllResults(data.data))
+      apis.newerGet().then(data => commit.setNewer(data.data))
+      apis.benchmarkQueueGet().then(data => commit.setQueue(data.data))
     }
   }
 })
