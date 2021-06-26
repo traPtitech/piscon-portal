089a12ff70dff0801b6b418025259d806c8a37e2
hijiki51 <hibiki0719euph@gmail.com>
2021-06-26 17:55:50 +0900

    fix type of store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,14 +1,15 @@
 import { createDirectStore } from 'direct-vuex'
-import { User, OAuth2Token } from '@traptitech/traq'
+import { User, OAuth2Token, MyUserDetail } from '@traptitech/traq'
 import TraqApis from '@/lib/apis/traq'
 import { setAuthToken } from '@/lib/apis/api'
+
 const { store, rootActionContext } = createDirectStore({
   state: {
-    me: null as User | null,
+    me: null as MyUserDetail | null,
     authToken: null as OAuth2Token | null
   },
   mutations: {
-    setMe(state, me: User) {
+    setMe(state, me: MyUserDetail) {
       state.me = me
     },
     setToken(state, data: OAuth2Token) {
@@ -19,8 +20,8 @@ const { store, rootActionContext } = createDirectStore({
   actions: {
     async fetchMe(context) {
       const { commit } = rootActionContext(context)
-      const { data: me } = await TraqApis.getMe()
-      commit.setMe(me)
+      const { data } = await TraqApis.getMe()
+      commit.setMe(data)
     }
   }
 })
