5f65a82b5671f899e7486bac28d357db56041a36
harsssh <mizuki.k.aa@m.titech.ac.jp>
2022-06-28 21:18:32 +0900

    update readme page

--- client/src/pages/admin/readme/Readme.vue
+++ client/src/pages/admin/readme/Readme.vue
@@ -2,27 +2,19 @@
   <va-content class="typography content">
     <div>
       <va-card class="col-md-12">
-        <va-card-title> <h3 class="h-fix">はじめに</h3> </va-card-title>
         <va-card-content>
           <div class="mb-4">
-            <h4>Q. Pisconとは？</h4>
+            <h4>PISUCON とは</h4>
             <p>
-              A.
-              <a href="https://trap.jp" target="_blank"
-              >東京工業大学デジタル創作同好会traP</a
-              >の部内ISUCONのことです。
+              <a href="https://trap.jp" target="_blank">東京工業大学デジタル創作同好会traP</a> の部内ISUCON のことです.
             </p>
           </div>
           <div class="mb-4">
-            <h4>Q. ISUCONとは？</h4>
+            <h4>ISUCONとは</h4>
             <p>
-              Iikanjini SpeedUp CONtestの略です。
-              <br />サーバーをチューニングして高いスコアを目指します。
-              <br />毎年秋くらいにコンテストを開催しています。 <br /><a
-                href="http://isucon.net/"
-                target="_blank"
-              >ISUCON公式サイト</a
-              >
+              Iikanjini SpeedUp CONtest の略で, サーバーをチューニングして高いスコアを目指す大会です.
+              <br />毎年夏に開催されています.<br />
+              <a href="http://isucon.net/" target="_blank">ISUCON 公式サイト</a>
             </p>
           </div>
           <div class="mb-4">
