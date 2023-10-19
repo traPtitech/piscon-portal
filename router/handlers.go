ec2ea8cbf49c6f4f4f5ef8eae8fb801343688163
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-10-19 19:52:45 +0900

    ベンチマーカー用のコマンドを修正

--- router/handlers.go
+++ router/handlers.go
@@ -48,7 +48,7 @@ func genPassword() string {
 // ベンチマーク実行コマンド（大会によって書き換えた）
 func formatCommand(ip string, allAddresses []string) string {
 	// TODO: target, all-addressesを環境変数で渡すようにする
-	return fmt.Sprintf("/isucari/bench/bench "+
+	return fmt.Sprintf("/isucari/bin/benchmarker "+
 		"--target-url=http://%s "+
 		"--data-dir=/isucari/initial-data", ip)
 }
