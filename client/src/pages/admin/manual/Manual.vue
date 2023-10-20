2f12275eb2cd205d1884002e12ea9cad2b2091df
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-10-20 12:39:08 +0900

    nginxの設定を修正

--- client/src/pages/admin/manual/Manual.vue
+++ client/src/pages/admin/manual/Manual.vue
@@ -27,7 +27,7 @@
         # ssl_certificate_key //etc/nginx/sites-available/isucari.confssl/privkey.pem;
 
         location / {
-                        proxy_set_header Host ;
+                        proxy_set_header Host $http_host;
                         proxy_pass http://127.0.0.1:8000;
         }
 }</code></pre>
