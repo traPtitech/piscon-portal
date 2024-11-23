f36fef5fac28972e911d7d34b546d907a18efda2
pikachu0310 <pikachu13711@gmail.com>
2024-11-23 09:56:54 +0900

    第二回用にAMIを変更

--- aws/aws.go
+++ aws/aws.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	imageId      = string("ami-0796be4f4814fc3d5") // isucon競技用サーバーのAMI
+	imageId      = string("ami-0ecfc02bf3af2d03e") // isucon競技用サーバーのAMI
 	InstanceType = types.InstanceTypeT2Medium      // isuconサーバーの種類(競技ごとにスペックが違う)
 	region       = string("ap-northeast-1")        // isuconサーバーのリージョン
 )
